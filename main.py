bbd28b084169ad9fb687ad83bb8703899bcea20c
Marcos556149 <83416004+Marcos556149@users.noreply.github.com>
2021-09-09 22:40:07 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from ClaseLista import Lista
+if __name__=='__main__':
+    unaLista=Lista(8)
+    unaLista.insertar(1,6)
+    unaLista.insertar(1,50)
+    unaLista.insertar(3,74)
+    unaLista.insertar(2,30)
+    unaLista.insertar(7,79)
+    unaLista.recorrer()
+
+
+
+
